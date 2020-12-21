63814b559843f91b279c2eaa7fb496ad7d6ccad0
AbhishekJha1994Outlook <75480987+AbhishekJha1994Outlook@users.noreply.github.com>
2020-12-21 14:32:06 +0530

    main

--- numpy.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a_list = list(range(1,100000))
-print(sum(a_list))
---
+++ ny.py
@@ -0,0 +1,6 @@
+import numpy
+# Creating array
+a = numpy.array([2,3,4])
+print("Creating an array")
+print(a)
+print("-------------------")
